706b48b1a7e81731fa23ceea850cab1a4041714e
HarrySwain <harry.j.swain@gmail.com>
2023-05-30 14:31:18 +1000

    added custom color primary off logo rgb and made buttons that color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'primary': {
+				100: '#f2f8e2',
+				200: '#d8e9a7',
+				300: '#bddb6d',
+				400: '#a3cc33',
+				500: '#749224',
+				600: '#749224',
+				DEFAULT: '#bddb6d',
+			}
+		},
 		extend: {},
 	},
 	plugins: [
